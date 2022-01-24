47d7e12459afc0d2449b60010d71579564696622
obisofdev <83822465+Obysoftt@users.noreply.github.com>
2022-01-24 11:54:36 -0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'pyobigram',
+  packages = ['pyobigram'],
+  version = '1.6',
+  description = 'Python Simple Telergam Api.',
+  author = 'obisoftdev',
+  author_email = '',
+  url = 'https://github.com/Obysoftt/pyobigram',
+  keywords = ['py', 'obigram'],
+  license = 'GPLv3',
+  classifiers = [],
+)
